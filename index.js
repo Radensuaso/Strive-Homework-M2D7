24ea74076e65f529e422de6d34eb6f8ae7c25f4a
Radensuaso <andrels9283@gmail.com>
2021-06-08 14:43:15 +0100

    Ex. 11 through 13 complete

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-/*
-        JS Exercises
-        EX11) Write a function to add a new link into the navbar
-        EX12) Write a function to change the color of the main title
-        EX13) Write a function to change the background of the jumbotron
-        EX14) Write a function to remove all the links under "Elsewhere"
-        EX15) Write a function to change the column size for heading in jumbotron
-        EX16) Write a function to remove the "Search" magnifying glass icon
-        EX17) Write a function to trim just the first 50 characters in the first paragraph for each blog post
-        EX18) Write a function and attach it to the "Newer" button, to add new Blog Post (just div and title)
-        EX19) Write a function and attach it to the "Older" button, to remove the last Blog Post
-        EX20) Write an alert with the name of the author every time the user hover with the mouse over an author name
-    */
+/* JS Exercises
+EX11) Write a function to add a new link into the navbar */
+function addNewLinkToNavbar() {
+  const navbar = document.querySelector("nav.nav")
+  const link = document.createElement("a")
+  link.classList.add("p-2", "text-muted")
+  link.href = "#"
+  link.innerText = "Another Link"
+
+  navbar.appendChild(link)
+}
+
+addNewLinkToNavbar()
+
+/* EX12) Write a function to change the color of the main title */
+function changeColorOfH1() {
+  const h1 = document.querySelector("div.jumbotron div.col-md-6 h1")
+  h1.style.color = "green"
+}
+
+changeColorOfH1()
+
+/* EX13) Write a function to change the background of the jumbotron */
+function changeJumbotronBackground() {
+  const jumbotron = document.querySelector("div.jumbotron")
+  jumbotron.style.backgroundImage =
+    "url('https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__480.jpg')"
+}
+
+changeJumbotronBackground()
+
+/* EX14) Write a function to remove all the links under "Elsewhere" */
+/* EX15) Write a function to change the column size for heading in jumbotron */
+/* EX16) Write a function to remove the "Search" magnifying glass icon */
+/* EX17) Write a function to trim just the first 50 characters in the first paragraph for each blog post */
+/* EX18) Write a function and attach it to the "Newer" button, to add new Blog Post (just div and title) */
+/* EX19) Write a function and attach it to the "Older" button, to remove the last Blog Post */
+/* EX20) Write an alert with the name of the author every time the user hover with the mouse over an author name */
